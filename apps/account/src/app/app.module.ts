b865795f1f1bb3750cc771b9ec80193afeb87986
SamariddinS <ssamar.96.96@gmail.com>
2023-02-16 23:06:47 +0300

    update account.app module

--- apps/account/src/app/app.module.ts
+++ apps/account/src/app/app.module.ts
@@ -1,8 +1,23 @@
-import { Module } from '@nestjs/common';
-import { UserModule } from './user/user.module';
-import { AuthModule } from './auth/auth.module';
+import { ConfigModule, ConfigService } from '@nestjs/config/dist';
+import { MongooseModuleAsyncOptions } from '@nestjs/mongoose';
 
-@Module({
-    imports: [UserModule, AuthModule],
-})
-export class AppModule {}
+export const getMongoConfig = (): MongooseModuleAsyncOptions => {
+    return {
+        useFactory: (configService: ConfigService) => ({
+            uri: getMongoString(configService),
+        }),
+        inject: [ConfigService],
+        imports: [ConfigModule],
+    };
+};
+
+const getMongoString = (configService: ConfigService) =>
+    'mongodb+srv://' +
+    configService.get('MONGO_USER') +
+    ':' +
+    configService.get('MONGO_PASSWORD') +
+    '@' +
+    configService.get('MONGO_HOST') +
+    '/' +
+    configService.get('MONGO_DATABASE') +
+    '?retryWrites=true&w=majority';
