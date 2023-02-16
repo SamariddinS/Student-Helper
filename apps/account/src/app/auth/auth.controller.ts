2d92fba0ba782e1fdebeefefb6206038123f664c
SamariddinS <ssamar.96.96@gmail.com>
2023-02-16 23:03:08 +0300

    update auth service and added contracts dto

--- apps/account/src/app/auth/auth.controller.ts
+++ apps/account/src/app/auth/auth.controller.ts
@@ -1,4 +1,23 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
+import { AccountLogin, AccountRegister } from '@student-helper/contracts';
+import { AuthService } from './auth.service';
 
 @Controller('auth')
-export class AuthController {}
+export class AuthController {
+    constructor(private readonly authService: AuthService) {}
+
+    @Post('register')
+    async register(
+        @Body() dto: AccountRegister.Request
+    ): Promise<AccountRegister.Response> {
+        return this.authService.register(dto);
+    }
+
+    @Post('login')
+    async login(
+        @Body() { email, password }: AccountLogin.Request
+    ): Promise<AccountLogin.Response> {
+        const { id } = await this.authService.validateUser(email, password);
+        return this.authService.login(id);
+    }
+}
--- apps/account/src/app/auth/auth.module.ts
+++ apps/account/src/app/auth/auth.module.ts
@@ -1,8 +1,12 @@
 import { Module } from '@nestjs/common';
+import { JwtModule } from '@nestjs/jwt';
+import { getJWTConfig } from '../configs/jwt.config';
+import { UserModule } from '../user/user.module';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
 
 @Module({
+    imports: [UserModule, JwtModule.registerAsync(getJWTConfig())],
     controllers: [AuthController],
     providers: [AuthService],
 })
