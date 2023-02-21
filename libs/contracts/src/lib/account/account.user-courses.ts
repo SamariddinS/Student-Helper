ba4b28d659bc7072915f9762d2402b2e4e123cc9
SamariddinS <ssamar.96.96@gmail.com>
2023-02-22 00:38:08 +0300

    types for query service

---
+++ libs/contracts/src/lib/account/account.user-courses.ts
@@ -0,0 +1,15 @@
+import { IUserCourses } from '@student-helper/interfaces';
+import { IsString } from 'class-validator';
+
+export namespace AccountUserCourses {
+    export const topic = 'account.user-courses.query';
+
+    export class Request {
+        @IsString()
+        id: string;
+    }
+
+    export class Response {
+        courses: IUserCourses[];
+    }
+}
---
+++ libs/contracts/src/lib/account/account.user-info.ts
@@ -0,0 +1,15 @@
+import { IUser } from '@student-helper/interfaces';
+import { IsString } from 'class-validator';
+
+export namespace AccountUserInfo {
+    export const topic = 'account.user-info.query';
+
+    export class Request {
+        @IsString()
+        id: string;
+    }
+
+    export class Response {
+        profile: Omit<IUser, 'passwordHash'>;
+    }
+}
