69fa3a4b64fa584198a23d9357edc189c1fe2a53
SamariddinS <ssamar.96.96@gmail.com>
2023-02-22 00:35:03 +0300

    formatted by js-ts-formatter

--- apps/account/src/app/auth/auth.service.ts
+++ apps/account/src/app/auth/auth.service.ts
@@ -7,42 +7,42 @@ import { UserRepository } from '../user/repositories/user.repository';
 
 @Injectable()
 export class AuthService {
-    constructor(
-        private readonly userRepository: UserRepository,
-        private readonly jwtService: JwtService
-    ) {}
+	constructor(
+		private readonly userRepository: UserRepository,
+		private readonly jwtService: JwtService
+	) { }
 
-    async register({ email, password, displayName }: AccountRegister.Request) {
-        const oldUser = await this.userRepository.findUser(email);
-        if (oldUser) {
-            throw new Error('Такой пользователь уже зарегистрирован');
-        }
-        const newUserEntity = await new UserEntity({
-            displayName,
-            email,
-            passwordHash: '',
-            role: UserRole.Student,
-        }).setPassword(password);
-        const newUser = await this.userRepository.createUser(newUserEntity);
-        return { email: newUser.email };
-    }
+	async register({ email, password, displayName }: AccountRegister.Request) {
+		const oldUser = await this.userRepository.findUser(email);
+		if (oldUser) {
+			throw new Error('Такой пользователь уже зарегистрирован');
+		}
+		const newUserEntity = await new UserEntity({
+			displayName,
+			email,
+			passwordHash: '',
+			role: UserRole.Student,
+		}).setPassword(password);
+		const newUser = await this.userRepository.createUser(newUserEntity);
+		return { email: newUser.email };
+	}
 
-    async validateUser(email: string, password: string) {
-        const user = await this.userRepository.findUser(email);
-        if (!user) {
-            throw new Error('Неверный логин или пароль');
-        }
-        const userEntity = new UserEntity(user);
-        const isCorrectPassword = await userEntity.validatePassword(password);
-        if (!isCorrectPassword) {
-            throw new Error('Неверный логин или пароль');
-        }
-        return { id: user._id };
-    }
+	async validateUser(email: string, password: string) {
+		const user = await this.userRepository.findUser(email);
+		if (!user) {
+			throw new Error('Неверный логин или пароль');
+		}
+		const userEntity = new UserEntity(user);
+		const isCorrectPassword = await userEntity.validatePassword(password);
+		if (!isCorrectPassword) {
+			throw new Error('Неверный логин или пароль');
+		}
+		return { id: user._id };
+	}
 
-    async login(id: string) {
-        return {
-            access_token: await this.jwtService.signAsync({ id }),
-        };
-    }
+	async login(id: string) {
+		return {
+			access_token: await this.jwtService.signAsync({ id }),
+		};
+	}
 }
