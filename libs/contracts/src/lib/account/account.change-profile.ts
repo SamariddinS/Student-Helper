1520d91e0b784b9173036f691c930f910382f846
SamariddinS <ssamar.96.96@gmail.com>
2023-02-22 00:37:40 +0300

    types for command service

---
+++ libs/contracts/src/lib/account/account.change-profile.ts
@@ -0,0 +1,16 @@
+import { IUser } from '@student-helper/interfaces';
+import { IsString } from 'class-validator';
+
+export namespace AccountChangeProfile {
+    export const topic = 'account.change-profile.command';
+
+    export class Request {
+        @IsString()
+        id: string;
+
+        @IsString()
+        user: Pick<IUser, 'displayName'>;
+    }
+
+    export class Response {}
+}
