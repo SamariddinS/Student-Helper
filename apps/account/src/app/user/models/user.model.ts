46ec49e9e22264cdea79104425a6fd724e90792b
SamariddinS <ssamar.96.96@gmail.com>
2023-02-22 00:36:16 +0300

    modified for command and query services

--- apps/account/src/app/user/models/user.model.ts
+++ apps/account/src/app/user/models/user.model.ts
@@ -1,25 +1,44 @@
 import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
-import { IUser, UserRole } from '@student-helper/interfaces';
-import { Document } from 'mongoose';
+import {
+	IUser,
+	IUserCourses,
+	PurchaseState,
+	UserRole,
+} from '@student-helper/interfaces';
+import { Document, Types } from 'mongoose';
+
+@Schema()
+export class UserCourses extends Document implements IUserCourses {
+	@Prop({ required: true })
+	courseId: string;
+
+	@Prop({ required: true, enum: PurchaseState, type: String })
+	purchaseState: PurchaseState;
+}
+
+export const UserCoursesSchema = SchemaFactory.createForClass(UserCourses);
 
 @Schema()
 export class User extends Document implements IUser {
-    @Prop()
-    displayName?: string;
+	@Prop()
+	displayName?: string;
 
-    @Prop({ required: true })
-    email: string;
+	@Prop({ required: true })
+	email: string;
 
-    @Prop({ required: true })
-    passwordHash: string;
+	@Prop({ required: true })
+	passwordHash: string;
 
-    @Prop({
-        required: true,
-        enum: UserRole,
-        type: String,
-        default: UserRole.Student,
-    })
-    role: UserRole;
+	@Prop({
+		required: true,
+		enum: UserRole,
+		type: String,
+		default: UserRole.Student,
+	})
+	role: UserRole;
+
+	@Prop({ type: [UserCoursesSchema], _id: false })
+	courses: Types.Array<UserCourses>;
 }
 
 export const UserSchema = SchemaFactory.createForClass(User);
--- apps/account/src/app/user/repositories/user.repository.ts
+++ apps/account/src/app/user/repositories/user.repository.ts
@@ -1,22 +1,30 @@
-import { InjectModel } from '@nestjs/mongoose';
-import { User } from '../models/user.model';
-import { Model } from 'mongoose';
 import { Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
 import { UserEntity } from '../entities/user.entity';
+import { User } from '../models/user.model';
 
 @Injectable()
 export class UserRepository {
 	constructor(
 		@InjectModel(User.name) private readonly userModel: Model<User>
-	) {}
+	) { }
 
 	async createUser(user: UserEntity) {
 		const newUser = new this.userModel(user);
 		return newUser.save();
 	}
 
+	async updateUser({ _id, ...rest }: UserEntity) {
+		return this.userModel.updateOne({ _id }, { $set: { ...rest } }).exec();
+	}
+
 	async findUser(email: string) {
 		return this.userModel.findOne({ email }).exec();
+	}
+
+	async findUserById(id: string) {
+		return this.userModel.findById(id).exec();
 	}
 
 	async deleteUser(email: string) {
