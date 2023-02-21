901715a0d1c0836607ddd3c72d30e65f7b767de1
SamariddinS <ssamar.96.96@gmail.com>
2023-02-21 18:27:49 +0300

    removed listening of port, app is initializing

--- apps/account/src/main.ts
+++ apps/account/src/main.ts
@@ -1,22 +1,11 @@
-/**
- * This is not a production server yet!
- * This is only a minimal backend to get started.
- */
-
 import { Logger } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
-
 import { AppModule } from './app/app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  const globalPrefix = 'api';
-  app.setGlobalPrefix(globalPrefix);
-  const port = process.env.PORT || 3333;
-  await app.listen(port);
-  Logger.log(
-    `🚀 Application is running on: http://localhost:${port}/${globalPrefix}`
-  );
+    const app = await NestFactory.create(AppModule);
+    await app.init();
+    Logger.log(`🚀 Application is running`);
 }
 
 bootstrap();
