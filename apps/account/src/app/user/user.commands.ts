3ea56f100f2a094edfae61340e3951c9c2814169
SamariddinS <ssamar.96.96@gmail.com>
2023-02-23 19:08:43 +0300

    added new command

--- apps/account/src/app/user/user.commands.ts
+++ apps/account/src/app/user/user.commands.ts
@@ -1,12 +1,13 @@
 import { Body, Controller } from '@nestjs/common';
-import { AccountChangeProfile } from '@student-helper/contracts';
-import { RMQRoute, RMQValidate } from 'nestjs-rmq';
+import { AccountBuyCourse, AccountChangeProfile, AccountCheckPayment } from '@student-helper/contracts';
+import { RMQRoute, RMQService, RMQValidate } from 'nestjs-rmq';
 import { UserEntity } from './entities/user.entity';
 import { UserRepository } from './repositories/user.repository';
+import { BuyCourseSaga } from './sagas/buy-course.saga';
 
 @Controller()
 export class UserCommands {
-	constructor(private readonly userRepository: UserRepository) { }
+	constructor(private readonly userRepository: UserRepository, private readonly rmaService: RMQService) { }
 
 	@RMQValidate()
 	@RMQRoute(AccountChangeProfile.topic)
@@ -23,5 +24,40 @@ export class UserCommands {
 
 		await this.userRepository.updateUser(userEntity);
 		return {};
+	}
+
+	@RMQValidate()
+	@RMQRoute(AccountBuyCourse.topic)
+	async buyCourse(@Body() { userId, courseId }: AccountBuyCourse.Request): Promise<AccountBuyCourse.Response> {
+		const existedUser = await this.userRepository.findUserById(userId);
+		if (!existedUser) {
+			throw new Error('Такого пользователя нет')
+		}
+
+		const userEntity = new UserEntity(existedUser);
+		const saga = new BuyCourseSaga(userEntity, courseId, this.rmaService);
+		const { user, paymentLink } = await saga.getState().pay();
+
+		await this.userRepository.updateUser(user);
+
+		return { paymentLink }
+	}
+
+	@RMQValidate()
+	@RMQRoute(AccountCheckPayment.topic)
+	async checkPayment(@Body() { userId, courseId }: AccountCheckPayment.Request): Promise<AccountCheckPayment.Response> {
+		const existedUser = await this.userRepository.findUserById(userId);
+		if (!existedUser) {
+			throw new Error('Такого пользователя нет')
+		}
+
+		const userEntity = new UserEntity(existedUser);
+		const saga = new BuyCourseSaga(userEntity, courseId, this.rmaService);
+		const { user, status } = await saga.getState().checkPayment();
+
+
+		await this.userRepository.updateUser(user);
+
+		return { status }
 	}
 }
